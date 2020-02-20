6b8b200e886b3f5dc486ef3e7f4c368ea887fb2d
Brittany Houtz <brittany.houtz@gmail.com>
2020-02-20 15:09:26 -0800

    footer-component

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {
+  Link
+} from 'react-router-dom';
+
+const Footer = () => (
+  <footer>
+    <Link to="/">About the Developers</Link>
+  </footer>
+);
+
+export default Footer;
+
+//The link will need to be changed to direct to About Us after that page has been built
