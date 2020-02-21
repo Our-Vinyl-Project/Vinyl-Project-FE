0860880ba215c5b9e2dff85c7a6d23a3d4e0d2ee
Brittany Houtz <brittany.houtz@gmail.com>
2020-02-20 17:07:11 -0800

    developer info scaffolded

---
+++ src/components/about/developers.js
@@ -0,0 +1,29 @@
+export default [
+  {
+    name: 'Aaron Mullan',
+    github: '',
+    linkedin: '',
+    image: ''
+  },
+
+  {
+    name: 'Brittany Houtz',
+    github: '',
+    linkedin: '',
+    image: ''
+  },
+
+  {
+    name: 'Casey Martell',
+    github: '',
+    linkedin: '',
+    image: ''
+  },
+
+  {
+    name: 'Sarah Mayfield',
+    github: '',
+    linkedin: '',
+    image: ''
+  },
+];
