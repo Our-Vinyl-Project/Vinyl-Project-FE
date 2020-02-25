14a502323de92f00450edad0d8645ee0d5be0c05
okFox <smayfield@gmail.com>
2020-02-25 13:40:18 -0800

    fetch from our be

--- src/services/getWantlist.js
+++ src/services/getWantlist.js
@@ -1,20 +1,12 @@
-
-//this is just for front end testing
-
 const getWantlist = username => {
   
-  return fetch(`OUR BACKEND`)
+  return fetch(`https://cors-anywhere.herokuapp.com/http://our-vinyl-project-staging.herokuapp.com/api/v1/items/${username}`)
     .then(res => Promise.all([res.ok, res.json()]))
     .then(([ok, data]) => {
       if(!ok){
         throw data;
       }
-      return data.wants.map(wants => ({
-        // id: wants.id,
-        // title: wants.title,
-        // url: wants.resource_url
-
-      }));
+      return data;
     });
 };
 
