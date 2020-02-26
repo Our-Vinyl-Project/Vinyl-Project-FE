071ba7ab67f3d0d92adb4ffbaf8ccc7add0981de
Brittany Houtz <brittany.houtz@gmail.com>
2020-02-26 14:03:01 -0800

    commented out code cleanup to get rid of errors and warnings

--- src/components/homepage/Homepage.js
+++ src/components/homepage/Homepage.js
@@ -1,17 +1,13 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useSelector } from 'react-redux';
 import { selectSaleItems } from '../../data/selectors/saleItemSelector';
 import { controlsSelector } from '../../data/selectors/controlsSelector';
-// import PropTypes from 'prop-types';
 import Controls from './Controls';
 import SalesList from '../sale/SalesList';
 function Homepage() {
   const { sortType, condition, maxPrice } = useSelector(controlsSelector);
 
   const wants = useSelector(state => selectSaleItems(state, sortType, condition, maxPrice));
-  console.log(wants, 'hiiiiii');
-  // useEffect(() => {
-  // }, [wants]);
 
   if(!wants){
     return (
