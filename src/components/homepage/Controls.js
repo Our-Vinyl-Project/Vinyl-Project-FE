69b872ae05e3386f79aa0bf91fcc4c059def9453
okFox <smayfield@gmail.com>
2020-02-26 10:44:36 -0800

    errant code neutralized

--- src/components/homepage/Controls.js
+++ src/components/homepage/Controls.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { getWants } from '../../data/actions/wantActions';
-import { setControls } from 
+
 
 const Controls = () => {
 
