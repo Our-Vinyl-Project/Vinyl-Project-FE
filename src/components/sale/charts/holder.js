ab5defe735ced17de561323e8fd6de2b507bc4c2
AaronMullan <aaroncmullan@gmail.com>
2020-02-28 11:59:31 -0800

    charts working better

--- src/components/sale/charts/holder.js
+++ src/components/sale/charts/holder.js
@@ -1,23 +1,4 @@
-import React, { useState, useEffect } from 'react';
-import { Chart } from 'react-google-charts';
-import getChartData from '../../../services/getChart.js';
-
-const PricesOverTimeChart = () => {
-  const [data, setData] = useState(null);
-  
-  useEffect(() => {
-    getChartData(374527)
-      .then (res => {
-        setData(res);
-      });
-  }, []);
-  if(!data) return null;
-  return (
-    <Chart
-      chartType="Line"
-      
-      rows={[
-        [new Date(data[0][0]), data[0][1]], 
+[new Date(data[0][0]), data[0][1]], 
         [new Date(data[1][0]), data[1][1]],
         [new Date(data[2][0]), data[2][1]],
         [new Date(data[3][0]), data[3][1]],
@@ -47,35 +28,4 @@ const PricesOverTimeChart = () => {
         [new Date(data[27][0]), data[27][1]],
         [new Date(data[28][0]), data[28][1]],
         [new Date(data[29][0]), data[29][1]],
-      ]}
-      
-      columns={[
-        {
-          type: 'date',
-          label: 'Time'
-        },
-        {
-          type: 'number',
-          label: 'Price in $USD',
-          legend: { position: 'bottom' },
-        }
-      ]}
-      options={
-        {
-          title: 'Price Change Over Time',
-          hAxis: {
-            title: 'Age',
-            viewWindow: { min: 0, max: 15 }
-          },
-          vAxis: { title: 'Weight', viewWindow: { min: 0, max: 15 } },
-          legend: 'none',
-          curveType: 'function',
-          colors: ['gray']
-        }
-      }
-      height={'200px'}
-      width={'600px'}
-    />
-  );
-};
-export default PricesOverTimeChart;
+        [new Date(data[30][0]), data[30][1]],
