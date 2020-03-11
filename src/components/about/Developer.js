3179ba904b1a69d9c5f34a274b77d9a2ec8ee113
Brittany Houtz <brittany.houtz@gmail.com>
2020-03-10 17:01:03 -0700

    updated gh/li links and made loading object bigger

--- src/components/about/Developer.js
+++ src/components/about/Developer.js
@@ -8,8 +8,8 @@ export const Developer = ({ name, linkedin, github, image }) => {
       <img  className={styles.devImg}src={image}/>
       <p>{name}</p>
       <section>
-        <a className={styles.devLinkGH} href={linkedin}>GitHub</a>・
-        <a className={styles.devLinkLI} href={github}>LinkedIn</a>
+        <a className={styles.devLinkGH} href={linkedin}>LinkedIn</a>・
+        <a className={styles.devLinkLI} href={github}>GitHub</a>
       </section>
     </li>
   );
